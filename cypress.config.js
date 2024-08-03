21b8ab34dc8f1297600a8ffb703a212383f87f09
IhGori <igor.vinicius@discente.univasf.edu.br>
2024-08-03 11:58:21 -0300

    feat: Adiciona ao config videoUpload

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: true,
+  videoUploadOnPasses: true,
   screenshotOnRunFailure: true, 
   e2e: {
     baseUrl: 'https://serverest.dev',
