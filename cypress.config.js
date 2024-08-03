f21ba79a1edd9e80c7f14c5aa80d1c5657b6936a
IhGori <igor.vinicius@discente.univasf.edu.br>
2024-08-03 11:53:40 -0300

    refactor: Ajusta config do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  screenshotOnRunFailure: true, 
   e2e: {
     baseUrl: 'https://serverest.dev',
-    video: true,
-    screenshotOnRunFailure: true, 
   },
 });
